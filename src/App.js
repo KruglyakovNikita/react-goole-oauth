189c457db12767cc94faac9a2e1316f00edbb9f5
Nikita <nikitakruglyakovwork@gmail.com>
2022-10-18 09:40:44 +0300

    Implement google auth

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import jwt_decode from 'jwt-decode';
+import { useState } from 'react';
+
+function App() {
+  const [user, setUser] = useState({});
+
+  function handleSignOut(e) {
+    e.preventDefault();
+    setUser({});
+    document.getElementById('signInDiv').hidden = false;
+  }
+
+  function handleCallbackResponse(response) {
+    var userObject = jwt_decode(response.credential);
+    setUser(userObject);
+    document.getElementById('signInDiv').hidden = true;
+  }
+  useEffect(() => {
+    /*global google*/
+    google.accounts.id.initialize({
+      client_id: '132370882321-gk7j93jaan79shpfso9nmjt9udg252nr.apps.googleusercontent.com',
+      callback: handleCallbackResponse,
+    });
+    google.accounts.id.renderButton(document.getElementById('signInDiv'), { theme: 'outline', size: 'large' });
+
+    google.accounts.id.prompt();
+  }, []);
+
+  return (
+    <div className='App'>
+      <div id='signInDiv'></div>
+      <div className='container'>
+        {Object.keys(user).length !== 0 && <button onClick={e => handleSignOut(e)}>Sign Out</button>}
+        {Object.keys(user).length !== 0 && (
+          <div>
+            <img src={user.picture} alt='your google img'></img>
+            <h3>{user.name}</h3>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
